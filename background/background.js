8868ed26d5e0f4d4d9a4954c06dd0d04892e9286
Zeno Zeng <zenoes@qq.com>
2013-11-19 21:04:40 +0800

    update background

--- background/background.js
+++ background/background.js
@@ -1,14 +1,15 @@
-var hash, plugins, head, i, plugin, script;
-hash = window.location.hash.replace(/#/g, '');
-plugins = hash.split(',');
-head = document.getElementsByTagName('HEAD').item(0);
-for (i = 0; i < plugins.length; i++) {
-    plugin = plugins[i];
-    if(plugin && plugin.length > 0) {
-        console.log(plugin);
-        script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = '../plugins/'+plugin+'/background.js';
-        head.appendChild(script);
+var plugins, head, i, plugin, script;
+var fireBackground = function(plugins) {
+    plugins = plugins.split(',');
+    head = document.getElementsByTagName('HEAD').item(0);
+    for (i = 0; i < plugins.length; i++) {
+        plugin = plugins[i];
+        if(plugin && plugin.length > 0) {
+            console.log(plugin);
+            script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = '../plugins/'+plugin+'/background.js';
+            head.appendChild(script);
+        }
     }
 }
